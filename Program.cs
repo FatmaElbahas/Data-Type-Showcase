8b66f5a270152ad5d20e7acf40e7ccaf33798e3f
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-08 00:13:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp105
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region myregion
+            int age = 10;
+            double commession = 13.4;
+            string name = "fatma";
+            bool isfemale = true;
+            Console.WriteLine( "integer "+age);
+            Console.WriteLine("string "+name);
+            Console.WriteLine("double "+commession);
+            Console.WriteLine("bool "+isfemale);
+            #endregion
+          
+        }
+    }
+}
